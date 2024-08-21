a78688c3c83776d1a189bd6b728dbae1c668c766
sergeytokarev <stokarev@novakidschool.com>
2024-08-21 17:31:32 +0700

    flutter back

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:flutter/services.dart';
 import 'dart:math';
 
 const testUrl =
@@ -31,65 +30,6 @@ class WebViewScreen extends StatefulWidget {
 
 /** dont works in iframe or without it **/
 class _WebViewScreenState extends State<WebViewScreen> {
-  // HTML-контент с iframe
-  final String htmlContentH5p = '''
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-      body, html {
-        height: 100%;
-        margin: 0;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: #f0f0f0;
-      }
-      iframe {
-        width: 80%;
-        height: 80%;
-        border: none;
-      }
-    </style>
-  </head>
-  <body>
-    <iframe 
-    allowfullscreen 
-    allow="fullscreen; autoplay"
-    src="https://qa-landing.novakidschool.com/h5p/wp-admin/admin-ajax.php?action=h5p_embed&id=8" />
-  </body>
-  </html>
-  ''';
-
-  final String htmlContentYoutube = '''
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-      body, html {
-        height: 100%;
-        margin: 0;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background-color: #f0f0f0;
-      }
-    </style>
-  </head>
-  <body>
-  <iframe width="560" height="315" 
-  src="https://www.youtube.com/embed/aBcH4on5WVc?si=9a4uoTAapQKSgTTd" 
-  title="YouTube video player" 
-  frameborder="0" 
-  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-  referrerpolicy="strict-origin-when-cross-origin" 
-  allowfullscreen></iframe>
-  </body>
-  </html>
-  ''';
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,18 +38,6 @@ class _WebViewScreenState extends State<WebViewScreen> {
             initialUrlRequest: URLRequest(
               url: WebUri(getCachebustedUrl(testUrl)),
             ),
-            // initialData: InAppWebViewInitialData(
-            //   data: htmlContentYoutube,
-            //   baseUrl: WebUri('https://qa-landing.novakidschool.com/'),
-            //   mimeType: 'text/html',
-            //   encoding: 'utf-8',
-            // ),
-            // initialData: InAppWebViewInitialData(
-            //   data: htmlContentYoutube,
-            //   baseUrl: WebUri('https://qa-landing.novakidschool.com/'),
-            //   mimeType: 'text/html',
-            //   encoding: 'utf-8',
-            // ),
             initialSettings: InAppWebViewSettings(
               javaScriptEnabled: true,
               mediaPlaybackRequiresUserGesture: false,
